4ea7e887303532ec137b28b180af260f6d81b5da
Breno Batista <brenojab@gmail.com>
2017-11-11 11:05:14 -0200

    - Sequência do curso do Alura.

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,39 +1,54 @@
+var tempoInicial = 3;
+
 var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length
-//console.log("Metallica\n" + frase);
-//console.log(tamFrase);
+var campo = $(".campo-digitacao");
 
 var tamFrase = $("#tamFrase");
-//console.log(tamFrase);
+var tempoRestante = $("#tempo-digitacao").text();
+var numPalavras = frase.split(" ").length();
 
 tamFrase.text(numPalavras)
 
-var campo = $(".campo-digitacao");
-campo.on("input",function(){
-    //console.log("clicked")
-    var conteudo = campo.val();
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $("#contador-palavras").text(qtdPalavras);
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
+$(function name() {
+    console.log("Scripts carregados");
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
 });
 
-var tempoRestante = $("#tempo-digitacao").text();
+function atualizaTamanhoFrase() {
+    campo.on("input", function () {
+        //console.log("clicked")
+        var conteudo = campo.val();
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+}
 
-campo.one("focus",function(){
-//console.log(tempoRestante);
-    var cronometroId = setInterval(function(){
-        tempoRestante--;
+function inicializaContadores() {
+    campo.one("focus", function () {
         //console.log(tempoRestante);
-        $("#tempo-digitacao").text(tempoRestante);
-        
-        if(tempoRestante < 1)
-        {
-            campo.attr("disabled",true);
-            clearInterval(cronometroId);
-        }
+        var cronometroId = setInterval(function () {
+            tempoRestante--;
+            //console.log(tempoRestante);
+            $("#tempo-digitacao").text(tempoRestante);
 
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroId);
+            }
+        }, 1000);
+    });
+}
 
-    },1000);
-    
-});
+function inicializaCronometro() {
+    $("#botao-reiniciar").click(function () {
+        campo.attr("disabled", false);
+        campo.val("");
+        $("#contador-palavras").text("0");
+        $("#contador-caracteres").text("0");
+        $("#tempo-digitacao").text(tempoInicial);
+    });
+}
